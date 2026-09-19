f0e710f88a23eb4369c36fad05a0403e4675c070
jonanv <jonan-vargas23@hotmail.com>
2026-09-19 10:11:01 -0500

    ✨ Add interface to object literal

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,19 +1,31 @@
-const ironman = {
-    firstname: 'Tony',
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+
+const ironman: Person = {
+    firstName: 'Tony',
     lastName: 'Stark',
     age: 45,
-    address: {
-        zipCode: '170002',
-        city: 'New York'
-    }
+    // address: {
+    //     zipCode: '170002',
+    //     city: 'New York'
+    // }
 };
 
-const spiderman = structuredClone(ironman);
+const spiderman: Person = {
+    firstName: 'Piter',
+    lastName: 'Parker',
+    age: 22,
+}
 
-spiderman.firstname = 'Peter';
-spiderman.lastName = 'Parker';
-spiderman.age = 22;
-spiderman.address.zipCode = '123009';
-spiderman.address.city = 'Manizales';
+// const spiderman = structuredClone(ironman);
+
+// spiderman.firstname = 'Peter';
+// spiderman.lastName = 'Parker';
+// spiderman.age = 22;
+// spiderman.address.zipCode = '123009';
+// spiderman.address.city = 'Manizales';
 
 console.log(ironman, spiderman);

cf7bd542c7917d0f1b2814a101fcda224cb0ebf6
jonanv <jonan-vargas23@hotmail.com>
2026-09-21 15:20:41 -0500

    🎨 Add interface to return of functions

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -1,7 +1,38 @@
-function greet(name: string) {
+function greet(name: string): string {
+    return `Hola ${ name }`;
+}
+
+const greet2 = (name: string) => {
     return `Hola ${ name }`;
 }
 
 const message = greet('Goku');
+const message2 = greet2('Vegeta');
 
 console.log(message);
+console.log(message2);
+
+interface User {
+    uid: string;
+    username: string;
+}
+
+function getUser(): User {
+    return {
+        uid: 'abc123',
+        username: 'el_papi23'
+    }
+}
+
+const getUser2 = () => {
+    return {
+        uid: 'abc123',
+        username: 'el_papi23'
+    }
+}
+
+const user = getUser();
+const user2 = getUser2();
+
+console.log(user);
+console.log(user2);

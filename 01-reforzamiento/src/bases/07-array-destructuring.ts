2c9d476d6ecc5a87cdf52121ac5b1d4cfeb56938
jonanv <jonan-vargas23@hotmail.com>
2026-09-23 17:24:08 -0500

    🎨 Add array with numbers and string for destructuring

--- 01-reforzamiento/src/bases/07-array-destructuring.ts
+++ 01-reforzamiento/src/bases/07-array-destructuring.ts
@@ -7,3 +7,11 @@ console.log({ p1, p2, p3 });
 const [ , , t3 ] = characterNames;
 
 console.log({ t3 });
+
+const returnsArrayFn = () => {
+    return ['ABC', 123] as const;
+}
+
+const [letters, numbers] = returnsArrayFn();
+
+console.log(numbers + 100);

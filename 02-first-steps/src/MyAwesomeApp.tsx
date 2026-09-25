155b8929f7f56ac7d04b9c3d709e01dc44ed2dca
jonanv <jonan-vargas23@hotmail.com>
2026-09-25 16:17:58 -0500

    🎨 Add variables and interpolation

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,20 @@
-export function MyAwesomeApp() {
+// export function MyAwesomeApp() {
+//     return (
+//         <>
+//             <h1>Fernando</h1>
+//             <h3>Herrera</h3>
+//         </>
+//     );
+// }
+
+export const MyAwesomeApp = () => {
+    const firstName = 'Johanny';
+    const lastName = 'Vargas';
+
     return (
         <>
-            <h1>Fernando</h1>
-            <h3>Herrera</h3>
+            <h1>{ firstName }</h1>
+            <h3>{ lastName }</h3>
         </>
     );
 }
--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -7,7 +7,7 @@ import { MyAwesomeApp } from './MyAwesomeApp';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FirstStepsApp />
+    {/* <FirstStepsApp /> */}
     <MyAwesomeApp />
   </StrictMode>,
 );

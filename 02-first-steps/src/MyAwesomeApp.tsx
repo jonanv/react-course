3566c09e9d624c1e9939754dc7d0a1c339a9524b
jonanv <jonan-vargas23@hotmail.com>
2026-09-25 16:33:02 -0500

    ✨ Add print variables types

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -10,11 +10,28 @@
 const firstName: string = 'Johanny';
 const lastName: string = 'Vargas';
 
+const games: string[] = ['Call of Duty', 'FIFA26', 'Assasind Creed'];
+
+const isActive: boolean = true;
+
+const obj: any = {
+    'zip-code': '123443',
+    'city': 'Manizales'
+}
+
 export const MyAwesomeApp = () => {
     return (
         <>
             <h1>{ firstName }</h1>
             <h3>{ lastName }</h3>
+
+            <p>{ games.join(', ') }</p>
+
+            <p>{ 2 + 2 }</p>
+
+            <p>{ isActive ? 'true' : 'false' }</p>
+
+            <p>{ JSON.stringify(obj) }</p>
         </>
     );
 }

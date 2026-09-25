bf68bff2ff0572aa04defc6c3a803f9b15c026ed
jonanv <jonan-vargas23@hotmail.com>
2026-09-25 16:03:13 -0500

    ✨ Create a new component

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,10 @@
+export function FirstStepsApp() {
+    return (
+        <>
+            <h1>Hola mundo!</h1>
+            <p>Esto es un párrafo</p>
+            
+            <button type="button">Click me!</button>
+        </>
+    );
+}
